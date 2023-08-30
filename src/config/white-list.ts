582979773e4d34e83528def81f820c3c551ce20b
Magese <magese@live.cn>
2023-08-30 14:46:35 +0800

    init

---
+++ src/config/white-list.ts
@@ -0,0 +1,17 @@
+import { type RouteLocationNormalized } from "vue-router"
+
+/** 免登录白名单（匹配路由 path） */
+const whiteListByPath: string[] = []
+
+/** 免登录白名单（匹配路由 name） */
+const whiteListByName: string[] = []
+
+/** 判断是否在白名单 */
+const isWhiteList = (to: RouteLocationNormalized) => {
+  // path 和 name 任意一个匹配上即可
+  const flag = whiteListByPath.indexOf(to.path) !== -1 || whiteListByName.indexOf(to.name as any) !== -1
+  console.log(flag)
+  return true
+}
+
+export default isWhiteList
